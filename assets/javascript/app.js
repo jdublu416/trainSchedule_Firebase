25ecabff55dd82629786a07645e7d3397f3cbaef
Janet Warren <33846232+jdublu416@users.noreply.github.com>
2018-03-12 21:55:36 -0400

    firebase code added to app.js file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,43 +1,53 @@
-$(document).ready(function(){
 
-    var schedule = [
+ var config = {
+    apiKey: "AIzaSyCGeKU7C220FlTTdmCejLM9MrsUB8MQ5Ig",
+    authDomain: "my-train-schedule-f45d7.firebaseapp.com",
+    databaseURL: "https://my-train-schedule-f45d7.firebaseio.com",
+    projectId: "my-train-schedule-f45d7",
+    storageBucket: "",
+    messagingSenderId: "293059558295"
+  };
+  firebase.initializeApp(config);
 
-        trainName ="",
-        
-        destination="",
+  var database = firebase.database();
 
-        frequency="",
+  var trainName;
+  var destination;
+  var firstTime;
+  var frequency;
 
-        nextArrival="",
-
-        minutesAway=""
-
-
-    ]
-
-       
+  $("#btnSubmit").on("click", function(event) {
+    event.preventDefault();
+    trainName = $("#name").val().trim();
+    destination = $("#destination").val().trim();
+    firstTime= $("#firstTime").val().trim();
+    frequency = $("#frequency").val().trim();
     
+    var newRow = $("<tr>");
+    var newTrainName = $("<td>");
+    newTrainName.text(trainName);
+    var newDestination = $("<td>");
+    newDestination.text(destination);
+    var newFirstTime = $("<td>");
+    newFirstTime .text(firstTime);
+    var newFrequency = $("<td>");
+    newFrequency.text(frequency);
+   
+    newRow.append(newTrainName);
+    newRow.append(newDestination);
+    newRow.append(newFirstTime);
+    newRow.append(newFrequency);
+
+    $("#schedContainer").append(newRow);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    database.ref().push({
+        trainName: trainName,
+        destination: destination,
+        frequency:frequency,
+        firstTime: firstTime,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+      });
+      $(".form-control").val("");
 });
-
 
